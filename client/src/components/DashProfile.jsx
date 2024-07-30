2a87b61263baecd6eea2f03eead8ff237da27c91
Chiluka-Rahul <rahulchiluka2511@gmail.com>
2024-07-31 02:32:14 +0530

    updated dashprofile

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -111,7 +111,7 @@ export default function DashProfile() {
     }
     try {
       dispatch(updateStart());
-      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+      const res = await fetch(`/server/user/update/${currentUser._id}`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
