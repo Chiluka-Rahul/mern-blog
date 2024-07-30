87f97c9e224b676263b8dfc720e197b01efeb5b1
Chiluka-Rahul <rahulchiluka2511@gmail.com>
2024-07-30 22:57:34 +0530

    created Dropdown functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,13 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link , useLocation} from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
+import { useSelector } from 'react-redux'
+
 
 export const Header = () => {
     const path = useLocation().pathname;
+    const {currentUser} = useSelector((state) => state.user);
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center text-sm sm:text-xl font-semibold'>
@@ -22,11 +25,37 @@ export const Header = () => {
             <AiOutlineSearch />
         </Button>
         <div className='flex md:order-2'>
-            <Link to="/sign-in">
-                <Button>
-                    Sign In
-                </Button>
-            </Link>
+            {currentUser ? (
+                <Dropdown
+                    arrowIcon ={false}
+                    inline
+                    label = {
+                        <Avatar 
+                            alt='user'
+                            img={currentUser.
+                                profilePicture}
+                            rounded
+                        />
+                    }
+                >
+                    <Dropdown.Header>
+                        <span className='block text-sm'>@{currentUser.username}</span>
+                        <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                    </Dropdown.Header>
+                    <Link to={'/dashboard?tab=profile'}>
+                        <Dropdown.Item>Profile</Dropdown.Item>
+                    </Link>
+                    <Dropdown.Divider />
+                    <Dropdown.Item>Sign out</Dropdown.Item>
+                </Dropdown>
+
+            ) : (
+                <Link to="/sign-in">
+                    <Button>
+                        Sign In
+                    </Button>
+                </Link>
+            )}
             <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
