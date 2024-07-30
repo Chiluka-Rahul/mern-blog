934a124be891e768aa6a42585ec5b767d49be100
Chiluka-Rahul <rahulchiluka2511@gmail.com>
2024-07-30 15:49:34 +0530

    changes made for backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
 
+mongoose.connect(process.env.MONGO_URL).then(
+    () => {console.log('Database connected')}
+)
+.catch(
+    (err) => {console.log(err)}
+)
 app.listen(3000, () => {
     console.log('server connected')
-    
+
 })
+
