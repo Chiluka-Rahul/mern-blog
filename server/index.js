f62f8f6b83c0dfdc4f8afc393c3c8602dcc4b68d
Chiluka-Rahul <rahulchiluka2511@gmail.com>
2024-08-25 18:28:44 +0530

    .

--- server/index.js
+++ server/index.js
@@ -34,7 +34,8 @@ app.use('/server/auth', authRoutes)
 app.use('/server/post', postRoutes)
 app.use('/server/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, 'client','dist','index.html'));
